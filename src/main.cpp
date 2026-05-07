982c6d4b71206a82c91bf57549d0d59cde708f94
Dawson Tennant <abu.exegesis@gmail.com>
2026-05-07 12:52:21 +0200

    Test out all the hardware LED connections

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // pinMode(5, OUTPUT); // Set pin D5 as an output
+  for(int dPin = 0; dPin < 14; dPin++) {
+    pinMode(dPin, OUTPUT);
+  }
+}
+
+void resetCathodes () {
+  for(int cathode = 9; cathode < 12; cathode++){
+    digitalWrite(cathode, HIGH);
+  }
+}
+
+void resetAnodes () {
+  for (int anode = 2; anode < 9; anode++){
+    digitalWrite(anode, LOW);
+  }
+}
+
+void selectCathode (int cathode) {
+  resetCathodes();
+  digitalWrite(cathode, LOW);
+}
+
+void selectAnode (int anode) {
+  resetAnodes();
+  digitalWrite(anode, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  for (int cathode = 9; cathode < 12; cathode++) {
+    selectCathode(cathode);
+
+    for (int anode = 2; anode < 9; anode++) {
+      selectAnode(anode);
+       delay(1000); // Wait for one second
+    }
+  }
+
 }
