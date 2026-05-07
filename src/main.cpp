0b95a5069a967541d550700606341e337f8d5d55
Dawson Tennant <abu.exegesis@gmail.com>
2026-05-07 14:28:46 +0200

    Make 4 distinct kinds of LED tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,34 @@
 #include <Arduino.h>
 
+#define SECOND 1000
+
+#define ANODE_UNITS_1 2
+#define ANODE_UNITS_2 3
+#define ANODE_UNITS_4 4
+#define ANODE_UNITS_8 5
+#define ANODE_TENS_1 6
+#define ANODE_TENS_2 7
+#define ANODE_TENS_4 8
+
+#define CATHODE_SS 9
+#define CATHODE_MM 10
+#define CATHODE_HH 11
+
+/* The idea here is for the cathode to be on as long as needed
+but also to scan between cathodes fast enough that it appears
+that all Hours Minutes Seconds LEDs are on all the time. This 
+may take some time to get it right. I can as a first pass do
+some googling to get some ballpark figures.
+*/
+#define cathodeNextScan 1000
+#define cathodeScanWait 1000
+
+// This next secion is ONLY for testing purposes
+#define LEDtest 0
+#define SSLEDs 1
+#define MMLEDs 2
+#define SSLEDs 3
+
 void setup() {
   // put your setup code here, to run once:
   // pinMode(5, OUTPUT); // Set pin D5 as an output
@@ -30,15 +59,41 @@ void selectAnode (int anode) {
   digitalWrite(anode, HIGH);
 }
 
-void loop() {
-
+// ONLY for testing purposes
+void testLEDs () {
   for (int cathode = 9; cathode < 12; cathode++) {
     selectCathode(cathode);
 
     for (int anode = 2; anode < 9; anode++) {
       selectAnode(anode);
-       delay(1000); // Wait for one second
+       delay(SECOND); // Wait for one second
     }
   }
+}
 
+void LEDs_on (int cathode) {
+  selectCathode (cathode);
+  for (int anode = 2; anode < 9; anode++){
+    digitalWrite(anode, HIGH);
+  }
+}
+
+void LEDs_off (int cathode) {
+  selectCathode (cathode);
+  for (int anode = 2; anode < 9; anode++){
+    digitalWrite(anode, LOW);
+  }
+}
+
+void testLEDsection (int section){
+  LEDs_on(section);
+  delay(SECOND);
+  LEDs_off(section);
+}
+
+void loop() {
+  testLEDs ();
+  testLEDsection(CATHODE_SS);
+  testLEDsection(CATHODE_MM);
+  testLEDsection(CATHODE_HH);
 }
